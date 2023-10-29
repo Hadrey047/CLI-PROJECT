d95a9fa4a430a18ab2fe31f30a36ba4a51759dd9
Adedamola Ajasa <azeemajasa@gmail.com>
2023-10-29 10:19:24 +0100

    updated changes

--- lib/cli.py
+++ lib/cli.py
@@ -1,16 +1,70 @@
 import click
+from dofas_kitchen import Users, Menus, Restaurant
+from create_db import Session
 
 
-@click.command()
+
+@click.group()
 def cli():
-    first_name = click.prompt("Enter an integer", type=click.STRING, default=0)
-    last_name= click.prompt("Enter another integer", type=click.STRING, default=0)
-    email = click.prompt("Enter another integer", type=click.STRING, default=0)
-    phone_number = click.prompt("Enter another integer", type=click.INT, default=0)
-    click.echo(f"{first_name} + {last_name} + {email} + {phone_number}")
+    # CLI tool for managing the models and database.
+    pass
 
+@cli.command()
+@click.argument('first_name')
+@click.argument('last_name')
+@click.argument('email')
+@click.argument('phone_number', type=int)
+def add_user(first_name, last_name, email, phone_number):
+    """Add a user to the database."""
+    session = Session()
+    user = Users(first_name=first_name, last_name=last_name, email=email, phone_number=phone_number)
+    session.add(user)
+    session.commit()
+    session.close()
+    print(f'User {first_name} added.')
 
-if __name__ == "__main__":
-    cli()
+@cli.command()
+@click.argument('breakfast')
+@click.argument('lunch')
+@click.argument('dinner')
+@click.argument('ethnic')
+def add_menu(breakfast, lunch, dinner, ethnic):
+    # To add a menu to the database
+    session = Session()
+    menu = Menus(breakfast=breakfast, lunch=lunch, dinner=dinner, ethnic=ethnic)
+    session.add(menu)
+    session.commit()
+    session.close()
+    print(f'Menu added: {breakfast}, {lunch}, {dinner}, {ethnic}.')
+
+@cli.command()
+@click.argument('soft_drink')
+@click.argument('alcoholic_drink')
+@click.argument('wine')
+@click.argument('user_id', type=int)
+@click.argument('menu_id', type=int)
+def add_restaurant(soft_drink, alcoholic_drink, wine, user_id, menu_id):
+    """Add a restaurant to the database."""
+    session = Session()
+    restaurant = Restaurant(
+        soft_drink=soft_drink, alcoholic_drink=alcoholic_drink, wine=wine, user_id=user_id, menu_id=menu_id
+    )
+    session.add(restaurant)
+    session.commit()
+    session.close()
+    print(f'Restaurant added: {soft_drink}, {alcoholic_drink}, {wine}.')
+
+@cli.command()
+def list_users_menus_restaurants():
+    """List users, their menus, and restaurants."""
+    session = Session()
+    users = session.query(Users).all()
+    for user in users:
+        print(f'User ID: {user.id}, Name: {user.first_name} {user.last_name}, Email: {user.email}, Phone: {user.phone_number}')
+        for menu in user.menus:
+            print(f'  Menu: Breakfast: {menu.breakfast}, Lunch: {menu.lunch}, Dinner: {menu.dinner}, Ethnic: {menu.ethnic}')
+        for restaurant in user.restaurants:
+            print(f'  Restaurant: Soft Drink: {restaurant.soft_drink}, Alcoholic Drink: {restaurant.alcoholic_drink}, Wine: {restaurant.wine}')
+    session.close()
 
 
--- lib/migrations/env.py
+++ lib/migrations/env.py
@@ -4,31 +4,29 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from dofas_kitchen import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
-
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
-fileConfig(config.config_file_name)
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
+from dofas_kitchen import Base
 target_metadata = Base.metadata
-target_metadata = None
-
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
 
-def run_migrations_offline():
+def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
 
     This configures the context with just a URL
@@ -40,7 +38,7 @@ def run_migrations_offline():
     script output.
 
     """
-    url = config.get_main_option("sqlite:///dofas.db")
+    url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -52,7 +50,7 @@ def run_migrations_offline():
         context.run_migrations()
 
 
-def run_migrations_online():
+def run_migrations_online() -> None:
     """Run migrations in 'online' mode.
 
     In this scenario we need to create an Engine
@@ -60,7 +58,7 @@ def run_migrations_online():
 
     """
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
+        config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
